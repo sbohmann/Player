d850465085b08944a6b705273ae23a4d4da41a0f
sbohmann <mail@sebastian-bohmann.com>
2018-01-07 16:03:23 +0100

    Initial Commit

---
+++ Player/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Player
+//
+//  Created by Sebastian Bohmann on 07.01.18.
+//  Copyright © 2018 Sebastian Bohmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
